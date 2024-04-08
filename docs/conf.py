8d3919f2f2817e8dcc9aee8eddac1aa9b3efed4d
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-04-07 21:35:47 -0500

    update sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,39 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
+project = "MATH/CODE"
+copyright = "2024, Alejandro Sánchez Yalí"
+author = "Alejandro Sánchez Yalí"
+release = "0.1"
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+    "nbsphinx",
+    "sphinx.ext.mathjax",
+    "sphinx_rtd_theme",
+    "sphinx_gallery.load_style",  # load CSS for gallery (needs SG >= 0.6)
+]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = (
+    ["_build", "Thumbs.db", ".DS_Store"],
+    "notebooks/tutorial*/*_empty.ipynb",
+)
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+master_doc = "index"
+
+highlight_language = "python3"
+
+nbsphinx_execute_arguments = [
+    "--InlineBackend.figure_formats={'svg', 'pdf'}",
+    "--InlineBackend.rc={'figure.dpi': 96}",
+]
