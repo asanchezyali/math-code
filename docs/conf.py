2025f4334d2d41da8d23e825b2dfd4ff1e0de248
Alejandro Sánchez Yalí <asanchezyali@gmail.com>
2024-04-08 08:03:57 -0500

    update workflow

--- docs/conf.py
+++ docs/conf.py
@@ -23,9 +23,12 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = (
-    ["_build", "Thumbs.db", ".DS_Store", "notebooks/tutorial*/*_empty.ipynb"]
-)
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "notebooks/tutorial*/*_empty.ipynb",
+]
 
 
 # -- Options for HTML output -------------------------------------------------
